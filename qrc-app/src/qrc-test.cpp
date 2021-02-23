510c2bc405637fdf24d7f73a2433f5e6fca1d438
jionzhao <jionzhao@qti.qualcomm.com>
2021-02-23 19:48:08 +0800

    qrc-app: Modify qrc test app to add user cmd.
    Change-Id: I97dd7ca47439548f3c5b67459842f2648234fb23

--- qrc-app/src/qrc-test.cpp
+++ qrc-app/src/qrc-test.cpp
@@ -17,43 +17,129 @@
 
 #define VERSION_STR         ("v1.0\n")
 
-int main(int argc, char *argv[])
-{
-   unsigned int i;
-   int fd = 0;
-   int cmd;
+typedef struct {
+   char *option_string;
+   unsigned int opt_str_len;
+   int  (*action_func)(void *param);
+   int  is_param_needed;
+} opt_func_map;
 
-   fd = open(QRC_DEV, O_RDWR);
+static void usage()
+{
+   printf("qrctest  -  check qrc driver ioctl .\n");
+   printf("   --help:\n");
+   printf("         prints this help.\n");
+   printf("   --version:\n");
+   printf("         prints the program version string.\n");
+   printf("   --reset:\n");
+   printf("         reset qrc hardware.\n");
+   printf("   --boot0 <pin-value 0|1>:\n");
+   printf("         set the qrc boot0 pin as <pin-value>.\n");
+   printf("\n");
+}
+
+static int print_help(void *p)
+{
+   usage();
+   return 0;
+}
+
+static int print_version(void *p)
+{
+   printf("Version: %s\n", VERSION_STR);
+   return 0;
+}
+
+static int do_reset(void *p)
+{
+   int cmd = QRC_REBOOT;
+   int fd = open(QRC_DEV, O_RDWR);
+
    if (fd < 0)
    {
       printf("Open Dev QRC Error!\n");
       return -1;
    }
 
-   printf("<--- Call QRC IOCTL REBOOT  --->\n");
-   cmd = QRC_REBOOT;
    if (ioctl(fd, cmd) < 0)
    {
-      printf("Call cmd QRC_REBOOT fail\n");
+      printf("qrc reboot fail\n");
+      close(fd);
       return -1;
    }
-   sleep(30);
 
-printf("<--- Call QRC IOCTL QRC_BOOT_TO_MEM  --->\n");
-   cmd = QRC_BOOT_TO_MEM;
-   if (ioctl(fd, cmd) < 0)
-   {
-      printf("Call cmd QRC_BOOT_TO_MEM fail\n");
-      return -1;
-   }
-   sleep(30);
-printf("<--- Call QRC IOCTL QRC_BOOT_TO_FLASH  --->\n");
-   cmd = QRC_BOOT_TO_FLASH;
-   if (ioctl(fd, cmd) < 0)
-   {
-      printf("Call cmd QRC_BOOT_TO_FLASH fail\n");
-      return -1;
-   }
    close(fd);
+   return 0;
+}
+
+static int do_boot0(void *p)
+{
+   char *opt = (char *)p;
+   unsigned int value = atoi(opt);
+   int cmd;
+   int fd = open(QRC_DEV, O_RDWR);
+
+   if (fd < 0)
+   {
+      printf("Open Dev QRC Error!\n");
+      return -1;
+   }
+
+   if (value)
+   {
+      cmd = QRC_BOOT_TO_MEM;
+      if (ioctl(fd, cmd) < 0)
+      {
+         printf("qrc ioctl fail\n");
+         return -1;
+      }
+   }
+   else
+   {
+      cmd = QRC_BOOT_TO_FLASH;
+      if (ioctl(fd, cmd) < 0)
+      {
+         printf("qrc ioctl fail\n");
+         return -1;
+      }
+   }
+
+   usage();
+   return 0;
+}
+
+opt_func_map opt_func_list[] = {
+   {"--help",           (sizeof("--help") - 1), &print_help,       0},
+   {"--version",        (sizeof("--version") - 1), &print_version,    0},
+   {"--reset",     (sizeof("--reset") - 1), &do_reset,    0},
+   {"--boot0",    (sizeof("--boot0") - 1), &do_boot0,   1},
+};
+
+int main(int argc, char *argv[])
+{
+   unsigned int i;
+
+   if((argc == 1) || (argc >= 4)) {
+      usage();
+      return 0;
+   }
+
+   for(i = 0; i < (sizeof(opt_func_list)/sizeof(opt_func_map)); i++) {
+      if (!(strncmp(argv[1], opt_func_list[i].option_string,
+                             opt_func_list[i].opt_str_len))) {
+         if (opt_func_list[i].is_param_needed == 1) {
+            if (argv[2]) {
+               opt_func_list[i].action_func(argv[2]);
+            } else {
+               usage();
+            }
+         } else {
+            opt_func_list[i].action_func(NULL);
+         }
+         return 0;
+      }
+   }
+
+   usage();
    return 0;
 }
