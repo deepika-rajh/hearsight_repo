80155f168f1c79f91e71fe55f25f04b01c7a6061
Chaochen Liang <chalia@qti.qualcomm.com>
2024-01-29 17:18:06 +0800

    robotics-prop : add new file for ocr_service
    Change-Id: Ia8a0b745410796912e02fea115dc24324284ca98

---
+++ ocr-service/ocr_ros2node/ocr_service/ocr_service.py
@@ -0,0 +1,11 @@
+'''
+Copyright (c) 2024 Qualcomm Technologies, Inc.
+All Rights Reserved.
+Confidential and Proprietary - Qualcomm Technologies, Inc.
+'''
+def main():
+    print('Hi from ocr_service.')
+
+
+if __name__ == '__main__':
+    main()
