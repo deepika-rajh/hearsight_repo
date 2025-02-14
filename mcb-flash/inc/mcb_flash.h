04cfd6dea6ec0e9d0e40852538951d54845f3fe6
jionzhao <jionzhao@qti.qualcomm.com>
2025-02-14 18:11:09 +0800

    robotics-prop: update mcb flash tool
    Change-Id: I7a322fdfaa007ed8c6a832c12500262723b9e8a5
    Signed-off-by: jionzhao <jionzhao@qti.qualcomm.com>

--- mcb-flash/inc/mcb_flash.h
+++ mcb-flash/inc/mcb_flash.h
@@ -1,34 +1,37 @@
 /************************************************************************
-* Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
-* SPDX-License-Identifier: BSD-3-Clause-Clear
-*************************************************************************/
+ * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
+ *************************************************************************/
 
 #ifndef _MCB_FLASH_H_
 #define _MCB_FLASH_H_
 
-#define VERSION_STR         ("v1.0\n")
-
-//#define NUTTX_BIN "/data/misc/mcb/nuttx.bin"
-#define NUTTX_BIN "./nuttx.bin"
-
-#define QRC_DEV  "/dev/qrc"
-
+#ifdef QRC_USER_DRIVER
+#define QRC_GPIOCHIP ("/dev/gpiochip0")
+#define QRC_RESETGPIO 168
+#define QRC_MAX_READ_SIZE 1024
+#define QRC_DEV "/dev/ttyHS1"
+#else
 /* IOCTL commands */
-#define QRC_IOC_MAGIC   'q'
+#define QRC_IOC_MAGIC 'q'
 /* Clear read fifo */
-#define QRC_FIFO_CLEAR  _IO(QRC_IOC_MAGIC, 1)
+#define QRC_FIFO_CLEAR _IO(QRC_IOC_MAGIC, 1)
 /* Reboot QRC controller */
-#define QRC_REBOOT      _IO(QRC_IOC_MAGIC, 2)
-/* QRC boot from memory */
+#define QRC_REBOOT _IO(QRC_IOC_MAGIC, 2)
+#define QRC_DEV "/dev/qrc"
+#endif
 
-#define CMD_FLASH   '1'
-#define CMD_BOOT    '2'
-
-int qrc_clean_rx_buff(int fd);
 void usage();
-int print_help(void *p);
-int print_version(void *p);
-int do_reboot(void *p);
-int do_flash(void *p, int* p_count);
+int print_help(void * p);
+int print_version(void * p);
+int do_reboot(void * p);
+int do_flash(void * p, int * p_count);
 
-#endif //_LIBQRC_H_
+int mcb_qrc_open(char * dev_path);
+void mcb_qrc_close(int fd);
+int mcb_qrc_clean_rx_buff(int fd);
+size_t mcb_qrc_read(int fd, char * data, size_t data_len);
+int mcb_qrc_write(int fd, char * data, size_t data_len);
+int mcb_reboot(int fd);
+
+#endif  //_LIBQRC_H_
